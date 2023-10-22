f899eb761667cd940d99cc01bcae269600fccd9a
Ansgar Hoyer <ansgar.hoyer@aoe.com>
2023-10-22 17:52:06 +0200

    add gmaps location

--- app/web/src/app/departures/page.tsx
+++ app/web/src/app/departures/page.tsx
@@ -1,10 +1,32 @@
-import { createClient, Location as HafasLocation } from "hafas-client";
-import { profile as rmvProfile } from "hafas-client/p/rmv";
 import { Suspense } from "react";
 import { z } from "zod";
 import { fromZodError } from "zod-validation-error";
+import { Client as GMapsClient, LatLng } from "@googlemaps/google-maps-services-js";
 
 import { Station } from "./components/Station";
+import { createClient, Location as HafasLocation } from "hafas-client";
+import { profile as rmvProfile } from "hafas-client/p/rmv";
+
+async function getAdressFromCoords(latlng: LatLng) {
+  const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
+  console.log("google maps trying to load", googleMapsApiKey);
+  if (googleMapsApiKey) {
+    try {
+      const gmapsClient = new GMapsClient({});
+      const result = await gmapsClient.reverseGeocode({
+        params: {
+          key: googleMapsApiKey,
+          latlng,
+        },
+      });
+
+      const adress = result.data.results[1].formatted_address;
+      return adress;
+    } catch (error) {
+      console.error("GMaps Error" + error);
+    }
+  }
+}
 
 export default async function Page({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
   const coordinateSchema = z.object({
@@ -24,7 +46,8 @@ export default async function Page({ searchParams }: { searchParams: { [key: str
     return <p>{errorMessage.toString()}</p>;
   }
   const { latitude, longitude, distance } = parsedInput.data;
-  console.info("Parsed URL Params: lat", latitude, "lon" + longitude, "dist" + distance);
+
+  const adress = await getAdressFromCoords({ latitude, longitude });
 
   const hafasClient = createClient(rmvProfile, "ansg.hoy+hafasClientApp@gmail.com");
 
@@ -35,10 +58,13 @@ export default async function Page({ searchParams }: { searchParams: { [key: str
   };
 
   const stations = await hafasClient.nearby(hafasLocation, { distance });
-  console.info("successful Hafas-Nearby Response with " + stations.length + " results");
+
   return (
     <Suspense fallback={<p>loading...</p>}>
       <main className="h-screen overflow-y-auto">
+        <div className="bg-slate-200">
+          <p className="text-xs text-center p-2">Ergebnisse für {adress}</p>
+        </div>
         <ul>{stations.map((station) => station.id && station.name && <Station key={station.id} hafasClient={hafasClient} stationId={station.id} stationName={station.name} />)}</ul>
       </main>
     </Suspense>
