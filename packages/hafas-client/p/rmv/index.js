e6f3fd404e52dffbf21a9381e2fbe8f5f464dafd
Ansgar Hoyer <ansgar.hoyer@aoe.com>
2023-10-22 15:31:22 +0200

    fix: try out import of json in hafas-client

--- packages/hafas-client/p/rmv/index.js
+++ packages/hafas-client/p/rmv/index.js
@@ -1,15 +1,13 @@
 // todo: use import assertions once they're supported by Node.js & ESLint
 // https://github.com/tc39/proposal-import-assertions
-import {createRequire} from 'module'
-const require = createRequire(import.meta.url)
 
-const baseProfile = require('./base.json')
-import {products} from './products.js'
+import baseProfile from "./base.json";
+import { products } from "./products.js";
 
 const profile = {
 	...baseProfile,
-	locale: 'de-DE',
-	timezone: 'Europe/Berlin',
+	locale: "de-DE",
+	timezone: "Europe/Berlin",
 
 	products,
 
@@ -18,8 +16,6 @@ const profile = {
 	radar: true,
 	refreshJourney: true,
 	reachableFrom: true,
-}
+};
 
-export {
-	profile,
-}
+export { profile };
